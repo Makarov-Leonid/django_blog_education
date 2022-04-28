b7e3553333ee119d4e837ec6a803d950255ad1bb
Leo <4.leo.makarov@gmail.com>
2022-04-28 15:42:17 +0300

    [+] complete lesson 4

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Post
 
 
 def posts_list(request):
-    n = 'oleg'
-    return render(request, 'blog/index.html', context={'name': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
